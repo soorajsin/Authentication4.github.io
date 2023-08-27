7a64198e8cf2c0f31a66c018dac986c34a57dee7
Sooraj <soorajsingh7505@gmail.com>
2023-08-27 17:34:57 +0530

    upload

--- server/DB/Connection.js
+++ server/DB/Connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-const db = "";
+const db = "mongodb+srv://soorajsingh7505:sooraj231@crud-app.4oebebt.mongodb.net/?retryWrites=true&w=majority";
 
 
 mongoose.connect(db, {
